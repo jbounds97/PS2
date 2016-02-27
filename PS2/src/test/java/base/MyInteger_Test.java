df8249c7eed6e32b2e9eb76f38485b71e5adf6c6
ohmymoses <ohmymoses@John-PC>
2016-02-26 20:42:42 -0500

    Another one

--- PS2/src/test/java/base/MyInteger_Test.java
+++ PS2/src/test/java/base/MyInteger_Test.java
@@ -10,34 +10,34 @@ import org.junit.Test;
 
 public class MyInteger_Test {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	static MyInteger object1 = new MyInteger(2);
+	static MyInteger object2 = new MyInteger(3);
+	static MyInteger object3 = new MyInteger(23);
 	
 	@Test
 	public void testIsEven(){
 		System.out.println("Test isEven");
-		MyInteger variable = new MyInteger();
-		variable.setiValue(20);
 		boolean bExpectedResult = true;
-		boolean bActualResult = variable.isEven();
-		assertEquals("test isEven() failed", bExpectedResult, bActualResult);
+		boolean bActualResult = object1.isEven();
+		assertEquals("test isEven() failed", bExpectewdResult, bActualResult);
 	}
 	
 	@Test
 	public void testIsOdd(){
 		System.out.println("Test isOdd");
-		MyInteger variable = new MyInteger();
-		variable.setiValue(15);
 		boolean bExpectedResult = true;
-		boolean bActualResult = variable.isOdd();
+		boolean bActualResult = object2.isOdd();
 		asserEquals("test isOdd() failed", bExpectedResult, bActualResult);
 	}
 	
 	@Test
 	public void testIsPrime(){
 		System.out.println("Test isOdd");
-		MyInteger variable = new MyInteger();
-		variable.setiValue(23);
 		boolean bExpectedResult = true;
-		boolean bActualResult = variable.isPrime();
+		boolean bActualResult = object3.isPrime();
 		asserEquals("test isPrime() failed", bExpectedResult, bActualResult);
 		}
 
